8a29935c6988bfd4e2cf23ccbd90c1c44e967153
Abbas <meerabbas110@gmail.com>
2024-02-02 11:57:28 +0530

    project to practice more for dashboard with library

---
+++ Dashboard-Tremor/src/App.jsx
@@ -0,0 +1,12 @@
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <main className="flex">
+      <Sidebar />
+      <div></div>
+    </main>
+  );
+}
+
+export default App;
